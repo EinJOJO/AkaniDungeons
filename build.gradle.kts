d37e478b6f579f9f0c59c90e0706031649395359
einjojo <einjojo@proton.me>
2024-07-22 16:17:11 +0200

    changed dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,12 +21,9 @@ repositories {
 
 dependencies {
     compileOnly(libs.paper)
-
-    implementation(libs.acf)
     compileOnly(libs.akanicore)
-    compileOnly("fr.minuskube.inv:smart-invs:1.2.7")
     compileOnly(libs.packeteventsspigot)
-    annotationProcessor(libs.acf)
+    compileOnly(libs.akaniutils)
     implementation(platform("com.intellectualsites.bom:bom-newest:1.44")) // Ref: https://github.com/IntellectualSites/bom
     compileOnly("com.fastasyncworldedit:FastAsyncWorldEdit-Core")
     compileOnly("com.fastasyncworldedit:FastAsyncWorldEdit-Bukkit") {
